ff868b67951ff1123352da67d98a1be32e51e423
蓝梦涵星 <qq2287160361@gmail.com>
2026-06-09 16:06:26 +0800

    Inherit some common Lineage stuff change to custom stuff

--- custom_pista.mk
+++ custom_pista.mk
@@ -12,7 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/realme/pista/device.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/custom/config/common_full_phone.mk)
 
 PRODUCT_NAME := custom_pista
 PRODUCT_DEVICE := pista
